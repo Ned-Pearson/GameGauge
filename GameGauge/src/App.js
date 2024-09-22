342f8e3f64681cb53dbc67767236e74f8c9d6801
Edward Pearson <s3844470@student.rmit.edu.au>
2024-09-22 14:29:31 +1000

    font

--- GameGauge/src/App.js
+++ GameGauge/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/homepage';
 import Header from './components/header';
 import Footer from './components/footer';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
 
 function App() {
   return (
