77e1684ed3c22fd1ed7e1630fbf4710ada7829cf
Edward Pearson <s3844470@student.rmit.edu.au>
2024-10-11 16:25:01 +1100

    removed unnecessary comment

--- GameGauge/react/src/pages/searchResults.js
+++ GameGauge/react/src/pages/searchResults.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useLocation, Link } from 'react-router-dom'; // Import Link for navigation
+import { useLocation, Link } from 'react-router-dom';
 import axios from 'axios';
 import './searchResults.css';
 
