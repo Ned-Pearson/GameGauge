7a1d946774534c3a58b2de57863a02bc1b788762
Edward Pearson <s3844470@student.rmit.edu.au>
2024-10-15 20:38:42 +1100

    Changed profile path to have the user prefix to avoid it attempting to query signin as a user

--- GameGauge/express/routes/logRoutes.js
+++ GameGauge/express/routes/logRoutes.js
@@ -1,19 +1,16 @@
-// routes/logRoutes.js
 const express = require('express');
 const router = express.Router();
 const authMiddleware = require('../middleware/authMiddleware');
 const { setLogStatus, getLogStatus, deleteLogStatus, getAllLogs, getLogsByUsername } = require('../controllers/logController');
+
+// Route for fetching logs by username (No authentication required)
+router.get('/logs/user/:username', getLogsByUsername); 
 
 // Protect routes with authentication middleware
 router.post('/logs', authMiddleware, setLogStatus);
 router.get('/logs/:gameId', authMiddleware, getLogStatus);
 router.delete('/logs/:gameId', authMiddleware, deleteLogStatus);
 
-router.get('/logs/user/:username', (req, res, next) => {
-    console.log('Request to fetch logs for:', req.params.username); // Add this to see if the request is reaching here
-    next();
-  }, getLogsByUsername);
-
-router.get('/logs', authMiddleware, getAllLogs); // New route to get all logs
+router.get('/logs', authMiddleware, getAllLogs);
 
 module.exports = router;
